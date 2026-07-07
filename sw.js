b13262d1f03d36a9fc22b45912dfda84ede05a6c
tjone7306 <129362142+tjone7306@users.noreply.github.com>
2026-07-06 21:10:07 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'thirds-cam-v5';
+const CACHE = 'thirds-cam-v6';
 const SHELL = ['.', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
